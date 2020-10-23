5829551fcb7d708aee3edac81079e8730780e567
maxim-speshilov <maxim.speshilov@gmail.com>
2020-10-23 04:03:03 +0300

    Deleted tiles4.tsx

--- Platformer/levels/tiles4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="tiles4" tilewidth="32" tileheight="32" tilecount="3200" columns="80">
- <image source="tileset32.png" width="2566" height="1308"/>
-</tileset>
